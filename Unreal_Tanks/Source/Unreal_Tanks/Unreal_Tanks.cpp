c208ba996e833c4b80f011c21fb1641c59c0b9f5
Pedro Calero <kalerolloko@gmail.com>
2019-04-14 15:27:50 +0200

    Unreal project created.

---
+++ Unreal_Tanks/Source/Unreal_Tanks/Unreal_Tanks.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Unreal_Tanks.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Unreal_Tanks, "Unreal_Tanks" );
---
+++ Unreal_Tanks/Source/Unreal_Tanks/Unreal_TanksGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Unreal_TanksGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL_TANKS_API AUnreal_TanksGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
